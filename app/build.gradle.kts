5fe09575a5d45573a6c8d181aa8fa69a74581a72
Svetoslav Izov <svetlio.bg@gmail.com>
2026-04-16 10:01:51 +0300

    Bump version to 1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.security.paniclock"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 4
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
